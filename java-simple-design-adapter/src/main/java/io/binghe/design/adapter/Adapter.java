ffff69a787635d24f758330e936a9a294d36be32
binghe001 <1028386804@qq.com>
2023-07-12 11:23:33 +0800

    feat: 第06章：适配器模式（Adapter）

---
+++ java-simple-design-adapter/src/main/java/io/binghe/design/adapter/Adapter.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2022-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.design.adapter;
+
+/**
+ * @author binghe(微信 : hacker_binghe)
+ * @version 1.0.0
+ * @description Target的实现类
+ * @github https://github.com/binghe001
+ * @copyright 公众号: 冰河技术
+ */
+public class Adapter implements Target{
+
+    private Adaptee adaptee;
+
+    public Adapter(Adaptee adaptee){
+        this.adaptee = adaptee;
+    }
+
+    @Override
+    public void adapteeMethod() {
+        adaptee.adapteeMethod();
+    }
+
+    @Override
+    public void adapterMethod() {
+        System.out.println("Adapter method!");
+    }
+}
