bd5ea401432592c81af3a961ef5bfaf78abf3757
binghe001 <1028386804@qq.com>
2023-07-13 21:57:02 +0800

    feat: 第08章：外观模式(Facade)

---
+++ java-simple-design-facade/src/main/java/io/binghe/design/facade/Facade.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2022-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.design.facade;
+
+/**
+ * @author binghe(微信 : hacker_binghe)
+ * @version 1.0.0
+ * @description Facade
+ * @github https://github.com/binghe001
+ * @copyright 公众号: 冰河技术
+ */
+public class Facade {
+
+    ServiceA sa;
+    ServiceB sb;
+    ServiceC sc;
+
+    public Facade() {
+        sa = new ServiceAImpl();
+        sb = new ServiceBImpl();
+        sc = new ServiceCImpl();
+    }
+
+    public void methodA() {
+        sa.methodA();
+        sb.methodB();
+    }
+
+    public void methodB() {
+        sb.methodB();
+        sc.methodC();
+    }
+
+    public void methodC() {
+        sc.methodC();
+        sa.methodA();
+    }
+}
