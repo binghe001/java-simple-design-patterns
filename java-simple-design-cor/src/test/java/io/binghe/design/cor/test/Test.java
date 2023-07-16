810c6c436ec0b636652ce13020636ef9f5592f73
binghe001 <1028386804@qq.com>
2023-07-16 23:01:08 +0800

    feat: 第13章：责任链模式(COR)

---
+++ java-simple-design-cor/src/test/java/io/binghe/design/cor/test/Test.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2022-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.design.cor.test;
+
+import io.binghe.design.cor.*;
+
+/**
+ * @author binghe(微信 : hacker_binghe)
+ * @version 1.0.0
+ * @description 测试类
+ * @github https://github.com/binghe001
+ * @copyright 公众号: 冰河技术
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        RequestHandler hr = new HRRequestHandler();
+        RequestHandler pm = new PMRequestHandler(hr);
+        RequestHandler tl = new TLRequestHandler(pm);
+
+        //team leader处理离职请求
+        Request request = new LeaveRequest();
+        tl.handleRequest(request);
+
+        System.out.println("===========");
+        //team leader处理加薪请求
+        request = new AddMoneyRequest();
+        tl.handleRequest(request);
+
+        System.out.println("========");
+        //项目经理上理辞职请求
+        request = new DimissionRequest();
+        pm.handleRequest(request);
+    }
+}
