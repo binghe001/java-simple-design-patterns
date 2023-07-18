4ce323b141cc3318d89dae03cc5f9a1f905c5a44
binghe001 <1028386804@qq.com>
2023-07-18 10:55:11 +0800

    feat: 第15章：解析器模式（Interpreter）

---
+++ java-simple-design-interpreter/src/main/java/io/binghe/design/interpreter/SimpleExpression.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2022-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.design.interpreter;
+
+/**
+ * @author binghe(微信 : hacker_binghe)
+ * @version 1.0.0
+ * @description Expression
+ * @github https://github.com/binghe001
+ * @copyright 公众号: 冰河技术
+ */
+public class SimpleExpression extends Expression{
+    @Override
+    public void interpret(Context ctx) {
+        System.out.println("这是普通解析器!");
+    }
+}
---
+++ java-simple-design-interpreter/src/test/java/io/binghe/design/interpreter/test/Test.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2022-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.design.interpreter.test;
+
+import io.binghe.design.interpreter.AdvanceExpression;
+import io.binghe.design.interpreter.Context;
+import io.binghe.design.interpreter.Expression;
+import io.binghe.design.interpreter.SimpleExpression;
+
+/**
+ * @author binghe(微信 : hacker_binghe)
+ * @version 1.0.0
+ * @description 测试类
+ * @github https://github.com/binghe001
+ * @copyright 公众号: 冰河技术
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Context ctx = new Context();
+        ctx.add(new SimpleExpression());
+        ctx.add(new AdvanceExpression());
+        ctx.add(new SimpleExpression());
+        for (Expression eps : ctx.getList()) {
+            eps.interpret(ctx);
+        }
+    }
+}
